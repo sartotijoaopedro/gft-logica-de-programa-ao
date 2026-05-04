f48628862f9a32de14cbf9d651bca9b4f62488e3
sartotijoaopedro <sartotijoaopedro@gmail.com>
2026-05-04 15:01:04 -0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-asdads
+let nomeJogador = "João";
+let xpJogador = 1001;
+let nivelJogador = "";
+
+if (xpJogador <= 1000) {
+    nivelJogador = "Ferro";
+} else if (xpJogador > 1000 && xpJogador <= 2000) {
+    nivelJogador = "Bronze";
+} else if (xpJogador > 2000 && xpJogador <= 5000) {
+    nivelJogador = "Prata";
+} else if (xpJogador > 5000 && xpJogador <= 7000) {
+    nivelJogador = "Ouro";
+} else if (xpJogador > 7000 && xpJogador <= 8000) {
+    nivelJogador = "Platina";
+} else if (xpJogador > 8000 && xpJogador <= 9000) {
+    nivelJogador = "Ascendente";
+} else if (xpJogador > 9000 && xpJogador <= 10000) {
+    nivelJogador = "Imortal";
+} else {
+    nivelJogador = "Radiante";
+}
+
+console.log(`O jogador ${nomeJogador} está no nível ${nivelJogador} com ${xpJogador} pontos de experiência!`);
